384057b0afc079024c4bcd4986982b3b3b399b70
Nikolay <qq1s@bk.ru>
2023-07-24 19:51:43 +0700

    add image downloading

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,7 +8,9 @@ import { promises as fs } from 'fs';
 import {
   test, expect, beforeEach,
 } from '@jest/globals';
-import { getFileName, downloadPage } from '../src/index.js';
+import {
+  getHtmlFileName, downloadPage, getFolderName, getAbsolutePath,
+} from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,38 +19,36 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 nock.disableNetConnect();
 
 const url = 'https://ru.hexlet.io/courses';
-const expectedFileName = getFileName(url);
+const expectedFileName = getHtmlFileName(url);
+const originHtmlFile = await fs.readFile(getFixturePath('origin.html'), 'utf-8');
 const expected = await fs.readFile(getFixturePath('ru-hexlet-io-courses.html'), 'utf-8');
+const fixtureImgPath = getFixturePath('ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png');
 let tempDir;
 
 beforeEach(async () => {
   tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, originHtmlFile)
+    .get('/assets/professions/nodejs.png')
+    .replyWithFile(200, fixtureImgPath);
 });
 
 test('download web page', async () => {
-  nock('https://ru.hexlet.io')
-    .get('/courses')
-    .reply(200, expected);
   const filePath = await downloadPage(url, tempDir);
   const result = await fs.readFile(filePath, 'utf-8');
   expect(result).toEqual(expected);
 });
 
 test('saved html file name', async () => {
-  nock('https://ru.hexlet.io')
-    .get('/courses')
-    .reply(200, expected);
   const filePath = await downloadPage(url, tempDir);
   const fileName = path.basename(filePath);
   expect(fileName).toEqual(expectedFileName);
 });
 
 test('created images folder', async () => {
-  nock('https://ru.hexlet.io')
-    .get('/courses')
-    .reply(200, expected);
-  await downloadPage(url, tempDir);
-  const filePath = getFolderName(url);
+  const resultPath = await downloadPage(url, tempDir);
+  const filePath = getAbsolutePath(path.join(tempDir, getFolderName(resultPath)));
   const stat = await fs.stat(filePath);
-  expect(stat.isDirectory).toBe(true);
+  expect(stat.isDirectory()).toBe(true);
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import axios from 'axios';
 import path from 'path';
+import prettier from 'prettier';
 import { promises as fs } from 'fs';
+import * as cheerio from 'cheerio';
 
-export const getFileName = (url) => {
+export const getHtmlFileName = (url) => {
   const urlWithoutProtocol = url.replace(/(^\w+:|^)\/\//, '');
   return `${urlWithoutProtocol.replace(/[^\w]/g, '-')}.html`;
 };
 
+export const getImageFileName = (url) => {
+  const formatName = (str) => str.replace(/\//g, '-');
+  return url.pathname ? formatName(url.pathname) : formatName(url);
+};
+
+export const getFolderName = (pathFile) => {
+  const { name } = path.parse(pathFile);
+  return `${name}_files`;
+};
+
+export const getAbsolutePath = (pathFile) => path.resolve(pathFile);
+
 export const downloadPage = async (url, dir) => {
-  const filePath = path.join(dir, getFileName(url));
-  const absolutePath = path.resolve(filePath);
+  const sourceUrl = new URL(url);
+  const htmlFileName = getHtmlFileName(url);
+  const absolutePath = getAbsolutePath(path.join(dir, htmlFileName));
+  const folderName = getFolderName(absolutePath);
+  const pathFolder = getAbsolutePath(path.join(dir, folderName));
+  let htmlResult;
+  let imgTags;
+  const imageLinks = [];
   return axios.get(url)
-    .then((response) => fs.writeFile(absolutePath, response.data))
+    .then(({ data }) => {
+      const $ = cheerio.load(data);
+      imgTags = $('img');
+      imgTags.each(function getImageLinks() {
+        imageLinks.push($(this).attr('src'));
+      });
+      htmlResult = $;
+    })
+    .then(() => fs.stat(pathFolder))
+    .catch(() => fs.mkdir(pathFolder))
+    .then(() => imageLinks.map((link) => {
+      const imageUrl = link.startsWith('http') ? new URL(link) : link;
+      const downloadImgUrl = link.startsWith('http')
+        ? `${imageUrl.origin}${imageUrl.pathname}`
+        : `${sourceUrl.origin}${imageUrl}`;
+      const localImageLink = `${folderName}/${sourceUrl.hostname.replace(/\./g, '-')}${getImageFileName(imageUrl)}`;
+      const absolutePathImage = getAbsolutePath(path.join(dir, localImageLink));
+      axios({
+        method: 'get',
+        url: downloadImgUrl,
+        responseType: 'stream',
+      })
+        .catch((error) => error)
+        .then((response) => {
+          fs.writeFile(absolutePathImage, response.data);
+        });
+      return localImageLink;
+    }))
+    .catch((error) => error)
+    .then((localImageLinks) => {
+      imgTags.each(function replaceImgLink(index) {
+        return htmlResult(this).attr('src', localImageLinks[index]);
+      });
+      return htmlResult.html();
+    })
+    .then((data) => prettier.format(data, { parser: 'html' }))
+    .then((formatedHtml) => fs.writeFile(absolutePath, formatedHtml))
     .catch((error) => error)
     .then(() => absolutePath);
 };
