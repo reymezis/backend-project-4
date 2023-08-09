8a5ee9fbd98e4aba58007b34a03bbd7750c043cb
Nikolay <qq1s@bk.ru>
2023-08-09 16:06:23 +0700

    fix default saving directory

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -7,7 +7,7 @@ program
   .version('0.0.1')
   .description('Page loader utility')
   .arguments('<url>')
-  .option('-o, --output [dir]', 'output dir', '/home/user/current-dir')
+  .option('-o, --output [dir]', 'output dir', `${process.cwd()}`)
   .action((url) => {
     downloadPage(url, program.opts().output)
       .then((result) => console.log(result))
